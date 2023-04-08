06f409588e12e15f8bd55e620d6540de5ee2c363
windowmacer <kirill@windowmaker.local>
2023-04-08 15:42:55 +0300

    lesson 1: done

---
+++ lesson01/README.md
@@ -0,0 +1,17 @@
+# Stanford Course Swift. Lesson 1.
+
+This lecture will be based on the creation of a program/game called  "Concentration (Memory)".
+
+Will learn it:
+- Create a project in the Xcode program
+- Create a visual part of the UI program
+- Connect the visual parts of the program to the code
+- Give functionality to visual parts by writing code
+
+In the code use:
+- Constants and Variables
+- Arrays
+- Functions
+- Property Observers
+- Optional
+- Optional Binding
--- lesson01/lesson1/ViewController.swift
+++ lesson01/lesson1/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+	var touches = 0 {
+		didSet{
+			touchLabel.text = "Touches: \(touches)"
+		}
 	}
-
-
+	let emojiCollection = ["🐫", "🦌", "🐫", "🦌"]
+	func flipButton(emoji: String, button: UIButton) {
+		if button.currentTitle == emoji {
+			button.setTitle("", for: .normal)
+			button.backgroundColor = #colorLiteral(red: 0, green: 0.565054059, blue: 0.3169590831, alpha: 1)
+		} else {
+			button.setTitle(emoji, for: .normal)
+			button.backgroundColor = #colorLiteral(red: 0.246004194, green: 0.6704418063, blue: 0.4856888652, alpha: 1)
+		}
+	}
+	
+	@IBOutlet var buttonCollection: [UIButton]!
+	@IBOutlet weak var touchLabel: UILabel!
+	@IBAction func buttonAction(_ sender: UIButton) {
+		touches += 1
+		if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+			flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+		}
+	}
 }
 
